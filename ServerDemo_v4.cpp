f8793aaef9e98457d2ab74471899eab2d739b309
HuiLanAi <310-we-aaa-1@163.com>
2018-05-09 18:52:32 +0800

    为了测试文件传输 对原有代码结构做了很大修改 注释掉了很多东西

--- ServerDemo_v4.cpp
+++ ServerDemo_v4.cpp
@@ -27,7 +27,7 @@ using namespace std;
 #pragma comment (lib, "Ws2_32.lib")
 // #pragma comment (lib, "Mswsock.lib")
 
-#define DEFAULT_BUFLEN 8188
+#define DEFAULT_BUFLEN 8100
 //不知道改这个数组的大小有没有用
 
 #define DEFAULT_PORT "27015"
@@ -123,6 +123,10 @@ int __cdecl main(void)
     //当前回合接收了文件 把recvBuf的值转移进来
     //下回合recvBuf接受了文件大小
     //再把tempFile写入
+    unsigned long transCount = 0;
+    unsigned long recvCount = 0;
+    FILE* fout = fopen("C:\\Users\\Mark.Wen\\Desktop\\SupremeChat\\ConsoleApplication1\\Debug\\1.mp4", "wb");
+    int leftSize = 0;
     
 
     while (1)
@@ -139,14 +143,14 @@ int __cdecl main(void)
         }
         //循环监听
 
-        cout << "正在等待接入..." << endl;
+        // cout << "正在等待接入..." << endl;
         ClientSocket = accept(ListenSocket, NULL, NULL);
         if (ClientSocket == INVALID_SOCKET)
         {
             cout << "接收失败" << endl;
             continue;
         }
-        cout << "收到一个消息: " << endl;
+        // cout << "收到一个消息: " << endl;
 
         if (ClientSocket == INVALID_SOCKET)
         {
@@ -167,11 +171,29 @@ int __cdecl main(void)
         //用于临时保存文件大小
         unsigned int fileSize = 0;
 
-        iResult = recv(ClientSocket, recvBuf, DEFAULT_BUFLEN, 0);
-        cout << recvBuf << endl;
+        if(transCount == 0)
+        //还没开始文件传输
+            iResult = recv(ClientSocket, recvBuf, DEFAULT_BUFLEN, 0);
+        if(transCount > 0 && recvCount < transCount)
+            {
+                // cout << "recvCount： " << recvCount <<endl;
+                // if(recvCount == 304) cout << "motherfuck" << endl;
+                // if(recvCount < transCount - 1)
+                //     iResult = recv(ClientSocket, recvBuf, DEFAULT_BUFLEN, MSG_WAITALL);
+                // else
+                iResult = recv(ClientSocket, recvBuf, DEFAULT_BUFLEN, 0);
+            }
+        else if(transCount > 0 && recvCount == transCount)
+        //接收最后一个报文的长度信息
+            iResult = recv(ClientSocket, recvBuf, DEFAULT_BUFLEN, 0);
+        else ;
+
+        if(leftSize > 0)
+            iResult = recv(ClientSocket, recvBuf, DEFAULT_BUFLEN, 0);
 
         if(recvBuf[2] != '|' && fileFlag == 0) fileFlag = 1;
         if(recvBuf[2] == '|') fileFlag = 0;
+        //先把要传送几次文件接收了
 
         if (iResult > 0)
         //这里为了测试文件传输 注释掉了很多无关动作 不然文件传输没法运行
@@ -182,53 +204,41 @@ int __cdecl main(void)
 
             //转移recvBuf
 
-            if(!fileFlag)
-            retVal = OutOfNetwork(recvBufToStr, userData, &mesCacheInfo);
-            //为了测试文件传输 不得不把OutOfNetwork()里的正常报文情况注释掉了
-
-            // if(fileFlag == 1)
-            // {
-            //     cout << (int) recvBuf[i] << endl;
-            // }
-
-
-            cout << "反馈消息： " << retVal << endl;
-
-            int res = send(ClientSocket, retVal.c_str(), retVal.length(), 0);
-            // 马上要修改 要适应多线程的修改 此处只返回发送成功的信号就行了 要改动OutOfNetwork函数
-
-            if (fileFlag == 1)
+            if (!fileFlag)
+            //如果是普通报文
+            //文件传输报文不再有send
             {
-                for(int i = 0; i < DEFAULT_BUFLEN; i ++)
+                retVal = OutOfNetwork(recvBufToStr, userData, &mesCacheInfo);
+                //为了测试文件传输 不得不把OutOfNetwork()里的正常报文情况注释掉了
+                int res = send(ClientSocket, retVal.c_str(), retVal.length(), 0);
+                // 马上要修改 要适应多线程的修改 此处只返回发送成功的信号就行了 要改动OutOfNetwork函数
+                if (res == SOCKET_ERROR)
                 {
-                    tempFile[i] = recvBuf[i];
+                    printf("发送失败: %d\n", WSAGetLastError());
+                    closesocket(ClientSocket);
+                    WSACleanup();
+                    return 1;
                 }
-                cout << "tempFile: \n" << tempFile << endl;
             }
 
-            //fileFlag为1 当前接收的是文件
-            //fileFlag为2 当前要接收文件大小
-            
-            if (res == SOCKET_ERROR)
+            if (fileFlag == 1 && transCount == 0)
             {
-                printf("发送失败: %d\n", WSAGetLastError());
-                closesocket(ClientSocket);
-                WSACleanup();
-                return 1;
-            }
+                // for(int i = 0; i < DEFAULT_BUFLEN; i ++)
+                // {
+                //     tempFile[i] = recvBuf[i];
+                // }
+                // cout << "tempFile: \n" << tempFile << endl;
+                retVal = "得到次数了";
+                transCount = atoi(recvBuf);
+                cout << retVal << " " << transCount << endl;
+                send(ClientSocket, retVal.c_str(), retVal.length(), 0);
+                //对应发送传输次数的sendMessageToServer
+                fileFlag  = 2;
 
-            // if (fileFlag)
-            // //区分文件报文还是消息/登录报文
-            // //下下之策 为了赶时间做demo
-            // {
-            //     string retVal = "接收到文件大小";
-            //     int fres = recv(ClientSocket, fileSizeStr, 50, 0);
-            //     send(ClientSocket, retVal.c_str(), retVal.length(), 0);
-            //     fileSize = atoi(fileSizeStr);
-            //     cout << "文件大小报文 " << fileSizeStr << endl;
-            //     memset(fileSizeStr, '\0', 50);
-            // }
+            }
+          
         }
+
         if (iResult == 0)
         //这里原来是else if (iResult == 0)
         {
@@ -253,20 +263,47 @@ int __cdecl main(void)
         //     return 1;
         // }
         // cout << recvBuf << endl;
-
-        if (fileFlag == 2)
+        if(recvCount == transCount && transCount != 0)
         {
-            fileSize = atoi(recvBuf);
-            //cout << "文件大小报文 " << tempFile << endl;
-            FILE* fout = fopen("C:\\Users\\Mark.Wen\\Desktop\\SupremeChat\\ConsoleApplication1\\Debug\\10.png", "wb");
-            fwrite(tempFile, fileSize, 1, fout);
-            fclose(fout);
-            memset(tempFile, '\0', DEFAULT_BUFLEN);
+            leftSize = atoi(recvBuf);
             memset(recvBuf, '\0', DEFAULT_BUFLEN);
+            recvCount ++;
+            
+        }
+        else if(leftSize != 0)
+        {
+            fwrite(recvBuf, leftSize, 1, fout);
+            fclose(fout);            
+            cout << "文件接收完毕" << endl;
+        }
+        else ;
+
+        if (transCount > 0 && recvCount < transCount && fileFlag != 2)
+        {
+            // fileSize = atoi(recvBuf);
+            //cout << "文件大小报文 " << tempFile << endl;
+            fwrite(recvBuf, DEFAULT_BUFLEN, 1, fout);
+            // if(recvCount % 400 == 0)
+            // cout << "发送进度: " << float(recvCount) / (float)(transCount) * 100 << endl;
+            cout << "recv: " << recvCount << endl;
+            // fclose(fout);
+            // memset(tempFile, '\0', DEFAULT_BUFLEN);
+            if(transCount == recvCount - 1)
+			{
+				cout << recvBuf << endl;
+				memset(recvBuf, '\0', DEFAULT_BUFLEN);
+			}
+            recvCount ++;
+            // continue;
         }
 
-        if(fileFlag == 1) fileFlag ++;
+        if(fileFlag >= 2 && fileFlag <= 255) fileFlag ++;
+        if(fileFlag == 255) fileFlag = 3;
+        
+
+
+        // if(fileFlag == 1) fileFlag ++;
         // cleanup
-        // closesocket(ClientSocket);
+        closesocket(ClientSocket);
     }
 }
