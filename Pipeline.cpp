9188d5d880960690dfbdddce590f1ade67234534
HuiLanAi <310-we-aaa-1@163.com>
2018-05-12 00:36:20 +0800

    完成了类和FILETOMEM的框架

--- Pipeline.cpp
+++ Pipeline.cpp
@@ -1,6 +1,7 @@
 #include "iostream"
 #include "windows.h"
 #include "fstream"
+#include "cstring"
 using namespace std;
 
 #define WIDTH 4
@@ -12,6 +13,8 @@ using namespace std;
 #define NOT_FULL 0
 #define FULL 1
 #define FILE_PATH "C"
+#define  SUCCESS 1
+#define FAIL 0
 
 class PublicCache
 {
@@ -24,6 +27,7 @@ public:
     int full;//缓冲区是否满的标识
     int round;//header和end相对位置的标识
     int lastTimeSize;//最后一个文件块的大小
+    unsigned long transTime;//文件整块传输的次数
 
     void zeroSpace()
     //给数组清零
@@ -51,7 +55,8 @@ public:
     void readCache(char* recv)
     //读缓存区
     //传入recv 将当前end对应的块写入recv
-    //最后修改end指针
+    //修改end指针
+    //最后判断是否为空
     {
 
     }
@@ -59,7 +64,8 @@ public:
     void writeCache(char* fileStr)
     //写缓存区
     //传入fileStr 将其值写入缓冲区header对应的位置块
-    //最后修改header指针
+    //修改header指针
+    //最后判断是否为满
     {
 
     }
@@ -78,20 +84,83 @@ int main()
 {
     PublicCache publicCache;
     publicCache.header = publicCache.end = publicCache.round = 0;
-    
+    publicCache.empty = EMPTY;
+    publicCache.full = NOT_FULL;
+    publicCache.available = UNLOCK;
+    publicCache.zeroSpace();
+    //--------------------------------------------------------
+    //缓冲区的初始化
+    //-------------------------------------------------------
+
+    /* 以二进制流打开文件
+        计算大小
+        计算整块发送次数和最后一次发送大小
+        修改publicCache里的对应属性 */
+    //上述功能朱强完成
 
     
 }
 
 DWORD WINAPI fileToMem(LPVOID para)
 {
+    FILE* file;//要操作的读入内存的文件指针
+    //朱强在此处修改 让file以二进制流打开FILE_PATH的文件
+    
     PublicCache* cachePtr = (PublicCache*) para;
-    //---------------------------------------------
-    //此处嵌入文件读写模块
-    //---------------------------------------------
-    while(cachePtr -> available)
-    {
 
+    unsigned long count = 0;
+    char fileStr[LENGTH] = { 0 };
+    int writeCond = SUCCESS;
+
+    while(count < cachePtr -> transTime)
+    {
+        if(writeCond == SUCCESS)
+            fread(fileStr, LENGTH, 1, file);
+        //先读一个
+        //再判断缓冲区可用不
+        //再上锁
+        
+        if (cachePtr -> available == UNLOCK && 
+            cachePtr -> full == NOT_FULL)
+        {
+            cachePtr -> available = LOCK;
+            cachePtr -> writeCache(fileStr);
+            //第一次写入
+            if(cachePtr -> getVaildSize() > 0)
+                cachePtr -> writeCache(fileStr);
+            //第二次写入
+            cachePtr -> available = UNLOCK;
+            //一个写入完成
+            writeCond = SUCCESS;
+            //写成功 可以从文件里读下一个块
+            count ++;
+        }
+        else
+        {
+            writeCond = FAIL;
+        }
     }
+    //-----------------------------------------------------
+    //整块传送
+    //------------------------------------------------------
+    writeCond = FAIL;
+    memset(fileStr, '\0', LENGTH);
+    fread(fileStr, cachePtr -> lastTimeSize, 1, file);
+    while(writeCond != SUCCESS)
+    {
+        if (cachePtr->available == UNLOCK &&
+            cachePtr->full == NOT_FULL)
+        {
+            cachePtr -> available = LOCK;
+            //对head当前所指向的块清零
+            //朱强完成该部分
+            cachePtr -> writeCache(fileStr);
+            cachePtr -> available = UNLOCK;
+            //一个写入完成
+            writeCond = SUCCESS;
+        }    
+    }
+    fclose(file);
+    exit(0);
 }
 
